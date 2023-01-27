d42ff38dab15429f6b039668de517a06d74c8294
campbellsibbald <81241828+campbellsibbald@users.noreply.github.com>
2023-01-27 00:31:36 -0500

    refractor
    refactored code

--- main.py
+++ main.py
@@ -1,135 +1,174 @@
 import pygame, sys
-pygame.init()
+import random
+from enum import Enum
+'''
+-> Make a grid system
+    -> Scalable to sreen
+    -> Set grid size
+    -> Add black bars depending on screen size
 
-# screen size
-size = swidth, sheight = 1500, 800
-# width and height for tiles
-width, height = 80,80
-speed = [2, 2]
-# background colour
-background = 122, 135, 146
+-> Make a User Interface
+    -> Buttons of specific action type
+        -> Buttons change UI to display all possible actions of that action type
+        -> When hovered display a description of what hat ction will do
+    -> Display hero icons on the side
+        -> Health Points and Actions
+    -> Turn order is displayed below hero icons
+        -> current turn and who's next
+        
+-> logs
+    -> logs of what has happend during combat
+'''
 
-#set the screen
-screen = pygame.display.set_mode(size)
-#DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-# name the game window and give it an icon
-pygame.display.set_caption('Rat Catchers')
-icon = pygame.image.load("simple_ratcatchers_icon.png")
-pygame.display.set_icon(icon)
-# clock for setting frames
-clock = pygame.time.Clock()
+class TileType(Enum):
+    '''
+    enumerator class for tile types, just makes the tile types more readable
+    e.g. TileType.GRASS for grass instead of the integer 0
+    '''
+    GRASS = 0
+    WATER = 1
+    WOOD = 2
 
+class GameObject:
+    '''
+    base class for anything on the grid system
+    everything has an x, y, width and height
+    '''
+    def __init__(self, x : float, y : float, w : float, h : float):
+        self.x, self.y, self.w, self.h = x, y, w, h
+    def handle_event(self, event : pygame.event.Event) -> None:
+        '''
+        handle event function of base class, will be implemented when inherited
+        '''
+        pass
 
-# a class for creating the grid of the map
+class Entity():
+    def __init__(self) -> None:
+        pass
+
+class Player():
+    '''
+    class for the player (contains multiple "players" eventually)
+    '''
+    def __init__(self, tile_size : float):
+        self.surface = pygame.transform.scale(pygame.image.load('Primalist_Sprite.png').convert_alpha(), (tile_size, tile_size))
+
+class Tile(GameObject):
+    '''
+    class for a tile in the game
+    '''
+    def __init__(self, x: float, y: float, tile_size : float, type : TileType):
+        super().__init__(x, y, tile_size, tile_size)
+        # storing the tile type (TileType.GRASS, TileType.WATER, etc.)
+        self.type = type
+        # the object in the tile, will be of Entity type
+        self.entity = None
+    def render(self, surface : pygame.Surface):
+        if self.entity:
+            surface.blit(self.entity.surface, (self.x * self.w, self.y * self.h))
+    def set_object(self, obj : Entity):
+        '''
+        sets the contained object to the passed Entity-type object
+        '''
+        self.entity = obj
+    def handle_event(self, event: pygame.event.Event, grid_position : tuple) -> None:
+        '''
+        the implemented method for handling events (look at GameObject class)
+        '''
+        # similar to switch case, I heard it was faster than if statements
+        match event.type:
+            case pygame.MOUSEBUTTONDOWN:
+                if pygame.Rect(self.x * self.w + grid_position[0], self.y * self.h + grid_position[1], self.w, self.h).collidepoint(event.pos):
+                    print('CLICKED TILE @ {} {}, TYPE {}, WITH : {}'.format(self.x, self.y, self.type, self.entity))
+
 class Grid:
-    # initilize function
-    def __init__(self, rows, cols, width, height, screen):
-        self.rows = rows
-        self.cols = cols
-        self.width = width
-        self.height = height
-        self.grid = [[None for _ in range(cols)] for _ in range(rows)]
-        self.screen = screen
-        self.rectangles = []
-    # function for adding a rectangle into the grid matrix
-    def add_rectangle(self, row, col, rectangle):
-        self.grid[row][col] = rectangle
-        self.rectangles.append(rectangle)
-    # function for draws the grid
-    def draw_grid(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                pygame.draw.rect(self.screen, (255, 255, 255), (j*self.width, i*self.height, self.width, self.height), 1)
-                if self.grid[i][j]:
-                    self.grid[i][j].draw()
-    # function for handling events (i.e. clicking)
-    def handle_events(self, event):
-        # finds which tile the click was in and calls on rectangle.interact()
-        for rectangle in self.rectangles:
-            if rectangle.rect.collidepoint(event.pos):
-                rectangle.interact()
-# a class mainly for creating tiles on the grid
-class Rectangle:
-    # initilize function
-    def __init__(self, name, description, x, y, width, height, color, screen):
-        self.name = name
-        self.description = description
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-        self.screen = screen
-    # function for when a tile is clicked
-    def interact(self):
-        print(f"You interact with {self.name}. {self.description}")
-    # function for drawing the rectangle
-    def draw(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
-    # function for setting new parameters for the rectangle
-    def set_rect(self, x, y, width, height):
-        self.rect = pygame.Rect(x, y, width, height)
-    # function for retreiving the rectangles parameters
-    def get_rect(self):
-        return self.rect
+    '''
+    grid class for storing all the tiles and drawing them
+    '''
+    def __init__(self, rows : int, cols : int, tile_size : float, tile_sprites : dict[TileType, pygame.Surface]) -> None:
+        self.rows, self.cols, self.SIZE = rows, cols, tile_size
+        self.tile_sprites = tile_sprites # a dictionary of surfaces to draw the tiles with, ties TileType with a Surface object
+        self.tile_size = tile_size # size of a tile
+        # filling a grid with random stuff using list comprehension
+        self.grid = [[Tile(x, y, self.tile_size, TileType.GRASS) for x in range(cols)] for y in range(rows)]
+        self.x, self.y = 0, 0
+        self.surface = pygame.Surface((self.cols * self.tile_size, self.rows * self.tile_size))
+    def handle_event(self, event : pygame.event.Event) -> None:
+        for row in self.grid:
+            for tile in row:
+                tile.handle_event(event, (self.x, self.y))
+    def set_tile(self, x : int, y : float, type : TileType) -> None:
+        self.grid[y][x].type = type
+    def render(self, surface : pygame.Surface) -> None:
+        for row in self.grid:
+            for tile in row:
+                # will draw the tile sprite based on what type it is (using the tile_sprites dictionary)
+                self.surface.blit(self.tile_sprites[tile.type], (tile.x*self.tile_size, tile.y*self.tile_size))
+                # draws the black rectangle around it
+                pygame.draw.rect(self.surface, (0, 0, 0), (tile.x*self.tile_size, tile.y*self.tile_size, self.tile_size, self.tile_size), 2)
+                tile.render(self.surface)
+        surface.blit(self.surface, (self.x, self.y))
 
-# initialize grid
-g = Grid(12, 8, swidth, sheight, screen)
+class RatCatchers:
+    def __init__(self) -> None:
+        # the display info
+        self.display_info = pygame.display.Info()
+        # the screen size constant
+        self.SIZE = (self.display_info.current_w, self.display_info.current_h)
+        # the main surface to draw onto
+        self.screen = pygame.display.set_mode(self.SIZE)
+        # adds an icon and title to the game
+        self.decorate_window()
+        # constants with grid size
+        ROWS, COLS = 8, 8
+        self.tile_size = self.SIZE[1] / ROWS # determining the size of 1 tile based on screen height
+        self.tile_dict = {}
+        self.load_tile_sprites()
+        self.clock = pygame.time.Clock() # clock for controlling time
+        self.BG_COLOR = pygame.Color(122, 135, 146) # the base background color
+        self.grid = Grid(ROWS, COLS, self.tile_size, self.tile_dict) # instantiating the new grid object
+        self.grid.x = (self.SIZE[0] - (COLS * self.tile_size))/2
+        self.player = Player(self.tile_size)
+        self.grid.grid[3][3].set_object(self.player)
+        self.done = False # to run the game later
+    def load_tile_sprites(self) -> None:
+        # creating the tile dictionary to tie a tile type to an image
+        self.tile_dict[TileType.GRASS] = pygame.transform.scale(pygame.image.load("grass_tile.jpg").convert(), (self.tile_size, self.tile_size))
+        self.tile_dict[TileType.WATER] = pygame.transform.scale(pygame.image.load("water_tile.jpg").convert(), (self.tile_size, self.tile_size))
+        self.tile_dict[TileType.WOOD] = pygame.transform.scale(pygame.image.load("wood_tile.jpg").convert(), (self.tile_size, self.tile_size))
+    def decorate_window(self) -> None:
+        pygame.display.set_caption('Rat Catchers')
+        self.icon = pygame.image.load("simple_ratcatchers_icon.png").convert()
+        pygame.display.set_icon(self.icon)
+    def run(self) -> None:
+        '''
+        the main loop of the game
+        '''
+        while not self.done:
+            self.handle_events() # handle events
+            self.tick() # update any logic
+            self.render() # draw the game
 
-# create and add rectangles to grid
-for i in range(4,12):
-    for j in range(8):
-        rect = Rectangle(f"{i},{j}", "", j*width, i*height, width, height, (122, 135, 146), screen)
-        g.add_rectangle(i, j, rect)
+    def handle_events(self) -> None:
+        for event in pygame.event.get():
+            print(event)
+            match event.type:
+                case pygame.QUIT:
+                    self.done = True
+            self.grid.handle_event(event) # tells the grid to handle the event
+    def tick(self) -> None:
+        # GAME LOGIC, NOTHING REALLY HERE
+        pass
+    def render(self) -> None:
+        self.screen.fill(self.BG_COLOR) # fill the background with a color first
+        self.grid.render(self.screen) # tells the grid to render to screen
+        # eventually, add more objects that also have some sort of render function to them
+        # and then add them here to render
+        self.clock.tick()
+        pygame.display.update() # updates the screen
 
-#images fpr each tile
-grass_tile = pygame.image.load("grass_tile.jpg")
-grass_tile = pygame.transform.scale(grass_tile, (width, height))
-water_tile = pygame.image.load("water_tile.jpg")
-water_tile = pygame.transform.scale(water_tile, (width, height))
-wood_tile = pygame.image.load("wood_tile.jpg")
-wood_tile = pygame.transform.scale(wood_tile, (width, height))
-
-#create the player
-player = pygame.image.load("Primalist_Sprite.png")
-player = pygame.transform.scale(player, (width, height))
-playerrect = Rectangle("PlayerCharacter1", "look at lil guy go", 320, 0, width, height, (122, 135, 146), screen)
-g.add_rectangle(0, 0, playerrect)
-
-#main loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        # detects if the user has clicked
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            g.handle_events(event)
-
-    # the background
-    screen.fill(background)
-    
-    # creates the grid
-    g.draw_grid()
-
-    # draws the tiles
-    for row in range(8):
-        for col in range(4,12):
-            # draws the water tiles
-            if (col >= 12 - row and col <= 14 - row):
-                screen.blit(water_tile, (col*width, row*height))
-            # draws the grass tiles
-            else:
-                screen.blit(grass_tile, (col*width, row*height))
-            # draws the wood tiles
-            if ((row == 4 and (col >= 7 and col <= 9)) or (row == 5 and (col >= 8 and col <= 10))):
-                screen.blit(wood_tile, (col*width, row*height))
-            
-    # draws the grid
-    for y in range(height):
-        for x in range(width):
-            rect = pygame.Rect(x*width, y*height, width, height)
-            pygame.draw.rect(screen, (0,0,0), rect, 1)
-
-    # draws the playerA
-    screen.blit(player, playerrect)
-
-    # updates the parts of the screen that need to be
-    pygame.display.flip()
-    # caps the fps at 60
-    clock.tick(60)
+if __name__ == '__main__':
+    pygame.init() # initialize pygame
+    game = RatCatchers() # create an instance of the game
+    game.run() # call the run method (contains the main loop)
+    pygame.quit() # quit pygame
